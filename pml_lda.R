d3c1e803eacd1811d2f3e1934ff8b291cfe48a78
Artem Artemiev <art.art.v@gmail.com>
2014-07-27 22:31:38 +0400

    Add markdown and html files

--- pml_lda.R
+++ pml_lda.R
@@ -1,15 +1,12 @@
-
 require(caret)
-
-## without factors
 
 ids <- which(sapply(training, class) == 'factor')
 wof.testing  <- testing[, -ids]
 wof.training <- training[, -ids]
 
-wof.pca <- preProcess(wof.training, method = 'pca', thresh = .95)
-wof.pc.train <- predict(wof.pca, wof.training)
-wof.pc.test  <- predict(wof.pca, wof.testing)
+pca <- preProcess(wof.training, method = 'pca', thresh = .95)
+pc.train <- predict(pca, wof.training)
+pc.test  <- predict(pca, wof.testing)
 
 control <- trainControl(method = "cv", number = 10)
 
@@ -20,7 +17,7 @@ wof.model1 <- train(
 
 wof.model2 <- train(
     training$classe ~ .,
-    data      = wof.pc.train,
+    data      = pc.train,
     method    = 'lda',
     trControl = control)
 
--- pml_visualization.R
+++ pml_visualization.R
@@ -1,10 +1,6 @@
 require(caret)
 require(ggplot2)
 
-ids <- which(sapply(training, class) == 'factor')
-wof.testing  <- testing[, -ids]
-wof.training <- training[, -ids]
-
 ggplot(data = training) + 
     aes(x = num_window, y = pitch_forearm, color = classe) +
     geom_point(alpha=0.3)
@@ -17,9 +13,13 @@ ggplot(data = training) +
     aes(x = num_window, y = magnet_dumbbell_z, color = classe) +
     geom_point(alpha=0.3)
 
-wof.pca <- preProcess(wof.training, method = 'pca', thresh = .95)
-wof.pc.train <- predict(wof.pca, wof.training)
-wof.pc.test  <- predict(wof.pca, wof.testing)
+ids <- which(sapply(training, class) == 'factor')
+wof.testing  <- testing[, -ids]
+wof.training <- training[, -ids]
+
+pca <- preProcess(wof.training, method = 'pca', thresh = .95)
+pc.train <- predict(pca, wof.training)
+pc.test  <- predict(pca, wof.testing)
 
 ggplot(data = wof.pc.train) + 
     aes(x = PC1, y = PC2, color = training$classe) +
